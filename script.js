7c36a29c89f1aa14b0b9308fb8c5c8fb77e7b82c
ismailPervez <freelanceismail001@gmail.com>
2021-08-02 23:44:53 -0700

    added the cart functionality, to update cart and localStorage

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log('working');
+// get cart items from local storage, if any - when web app first runs
+function updateCart() {
+    if (localStorage.getItem('pizza-palace-cart')) {
+        var inCartItems = JSON.parse(localStorage.getItem('pizza-palace-cart'));
+        var cartItemsContainer = $('.cart-container .cart-items');
+        var itemsCount = $('.cart-icon .items-count');
+        var totalPriceContainer = $('.cart-container .total-price span');
+        itemsCount.html(inCartItems.length);
+    
+        var containerInnerHtml = '';
+        var totalPrice = 0;
+        var id = 1;
+        for (let i = 0; i < inCartItems.length; i++) {
+            containerInnerHtml += `
+                <li class="item" id="item-${id}">
+                    ${inCartItems[i].size} pizza with ${inCartItems[i].crust} crust and ${inCartItems[i].toppings} toppings total of ksh.${inCartItems[i].totalPrice}
+                    <i class="fas fa-times"></i>
+                </li>
+            `
+            id += 1;
+            totalPrice += inCartItems[i].totalPrice;
+        }
+    
+        cartItemsContainer.html(containerInnerHtml);
+        totalPriceContainer.html(totalPrice);
+    }
+    
+    else {
+        localStorage.setItem('pizza-palace-cart', JSON.stringify([]));
+    }
+}
+
+updateCart();
